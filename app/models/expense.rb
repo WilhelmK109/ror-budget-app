6cd25b5e9ed5bc3c03efae21d8402f10d2dbd89f
WilhelmK109 <kamulungaw@gmail.com>
2023-09-12 11:33:32 +0200

    Modify models by adding methods, validations and associations

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
-  belongs_to :user
-  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
