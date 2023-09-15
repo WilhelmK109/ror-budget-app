1e19ffbc5bb721fa796f8fa3b22bafdbc00d35b2
WilhelmK109 <kamulungaw@gmail.com>
2023-09-15 16:13:25 +0200

    Create the join table between categories and expenses

---
+++ db/migrate/20230914062950_create_join_table_categories_expenses.rb
@@ -0,0 +1,7 @@
+class CreateJoinTableCategoriesExpenses < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :categories, :expenses do |t|
+      t.index [:category_id, :expense_id]
+    end
+  end
+end
